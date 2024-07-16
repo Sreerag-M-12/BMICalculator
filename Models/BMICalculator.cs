1d9fed2051fc983a96a1e41e8dc43a34acd2eefa
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-16 19:52:04 +0530

    innit

---
+++ Models/BMICalculator.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIApp.Models
+{
+    internal class BMICalculator
+    {
+        private int _id;
+        private string _name;
+        private int _age;
+        private double _height;
+        private double _weight;
+        static double DEFAULT_WEIGHT = 50;
+        static double DEFAULT_HEIGHT = 1.55;
+
+        public int Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        public string Name
+        {
+            set { _name = value; }
+            get { return _name; }
+        }
+        public int Age 
+        { 
+            set { _age = value; }
+            get { return _age; }
+        }
+        public double Height
+        {
+            set { _height = value; }
+            get { return _height; }
+        }
+        public double Weight
+        {
+            set { _weight = value; }
+            get { return _weight; }
+        }
+
+        public BMICalculator(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Height = DEFAULT_HEIGHT;  
+            Weight = DEFAULT_WEIGHT;
+        }
+        public BMICalculator(int id, string name, int age, double height, double weight):this(id, name, age)
+        {
+            Weight = weight;
+            Height = height;
+        }
+        public static double CalculateBMI(BMICalculator user) { 
+            double BMI =  user.Weight / (user.Height*user.Height);
+            return BMI;
+        }
+
+        public string GetUserBMI(double BMI) {
+
+            if (BMI <= 18.4)
+            {
+                return "UnderWeight";
+            }
+            else if (BMI > 18.4 && BMI < 25)
+            {
+                return  "Normal";
+            }
+            else if (BMI >= 25 && BMI < 40)
+            {
+                return  "OverWeight";
+            }
+            else {
+                return "Obese";
+            }
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"=========User No {Id}===========");
+            Console.WriteLine($"User no: {Id}");
+            Console.WriteLine($"User Name: {Name}");
+            Console.WriteLine($"Height : {Height}");
+            Console.WriteLine($"Weight : {Weight}");
+            Console.WriteLine("==================================");
+            return " ";
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMIApp.Models;
+
+namespace BMIApp
+{
+    
+    internal class Program
+    {
+        static void GetBMIdetails(BMICalculator user)
+        {
+            double bmiScore = BMICalculator.CalculateBMI(user);
+            string output = user.GetUserBMI(bmiScore);
+            Console.WriteLine($"User {user.Id} BMI score is: {bmiScore} \n" +
+                $"User body status is {output}");
+        }
+        static void Main(string[] args)
+        {
+            BMICalculator user1 = new BMICalculator(101, "Ramesh", 23);
+            BMICalculator user2 = new BMICalculator(102, "Suresh", 23, 1.74, 90);
+            Console.WriteLine(user1);
+            GetBMIdetails(user1);
+            Console.WriteLine(user2);
+            GetBMIdetails(user2);
+        }
+    }
+}
